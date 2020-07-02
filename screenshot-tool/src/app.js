cce49063094ecffa82d068574a1e5d61e13c6892
Dan Bennett <dghbennett47@gmail.com>
2020-07-03 00:29:46 +0100

    chore: tidying

--- screenshot-tool/src/app.js
+++ screenshot-tool/src/app.js
@@ -1,9 +1,9 @@
 const debug = require('debug')('screenshot-tool');
 const screenshot = require('screenshot-desktop');
-const fs = require('fs'); // delete me
-const { sleep } = require('./utils');
 const AWS = require('aws-sdk');
+const { v4: uuid } = require('uuid');
 const Pusher = require('pusher');
+const { sleep } = require('./utils');
 
 const config = {
     sleepTime: 5000
@@ -15,51 +15,43 @@ const s3Client = new AWS.S3({
     secretAccessKey: process.env.S3_SECRETACCESSKEY
 });
 
+const pusher = new Pusher({
+    appId: '1029661',
+    key: 'cfa402a8ba9ab9d38c93',
+    secret: '09a70a855ed97bf8a9f2',
+    cluster: 'eu',
+    useTLS: true
+});
+
 
 (async () => {
 	try {
 		debug(`Starting with config: ${JSON.stringify(config, null, '\t')}`);
 
+        let lastImageName = null;
 		while (true) {
 			// take the screenshot every 5 seconds
 			await sleep(config.sleepTime);
             const imageBuffer = await screenshot();
 
             //upload buffer with current time appended
-            const params = {
+            const imageName =  `screenshot-${uuid()}.png`;
+            const { url: Location } = await s3Client.upload({
                 Bucket: 'system-design-photo-collection',
-                Key: `imageBuffer_${Date.now()}`,
+                Key: imageName,
                 Body: imageBuffer
-            }
-
-            const stored = await s3Client.upload(params).promise();
-            bufferURL = stored.Location;
-
-
-            // const downloadParams = {
-            //     Bucket: bucketName,
-            //     Key: filename
-            // }
-
-            // const data = await s3Client.getObject(downloadParams).promise();
-            // fs.writeFileSync(`./${filename}.png`, data.Body)
+            }).promise();
 
             // send pusher notification with new url
-            const pusher = new Pusher({
-                appId: '1029661',
-                key: 'cfa402a8ba9ab9d38c93',
-                secret: '09a70a855ed97bf8a9f2',
-                cluster: 'eu',
-                useTLS: true
-            });
+            await pusher.trigger('system-design', 'url-change', { url });
 
-            console.log(bufferURL);
-            pusher.trigger('system-design', 'url-change', {
-                'url': `${bufferURL}`
-            });
-
+            if (lastImageName) {
+                // TODO: delete the old image
+            }
+            lastImageName = imageName;
 		}
 	} finally {
-		debug('Shutting down...');
+        debug('Shutting down...');
+        // empty the s3 bucket
 	}
 })();
