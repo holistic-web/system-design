fbd09834bb88f5718f71b6edd32dbe9149f69ca6
Dan Bennett <dghbennett47@gmail.com>
2020-07-03 16:06:04 +0100

    feat: simplify and add empty bucket

--- screenshot-tool/src/app.js
+++ screenshot-tool/src/app.js
@@ -1,57 +1,52 @@
+require('dotenv').config();
 const debug = require('debug')('screenshot-tool');
 const screenshot = require('screenshot-desktop');
-const AWS = require('aws-sdk');
 const { v4: uuid } = require('uuid');
 const Pusher = require('pusher');
 const { sleep } = require('./utils');
+const S3Client = require('./S3Client');
+
+const S3 = new S3Client();
 
 const config = {
     sleepTime: 5000
 };
 
-
-const s3Client = new AWS.S3({
-    accessKeyId: process.env.S3_ACCESSKEY,
-    secretAccessKey: process.env.S3_SECRETACCESSKEY
-});
-
 const pusher = new Pusher({
-    appId: '1029661',
-    key: 'cfa402a8ba9ab9d38c93',
-    secret: '09a70a855ed97bf8a9f2',
+    appId: process.env.PUSHER_APPID,
+    key: process.env.PUSHER_KEY,
+    secret: process.env.PUSHER_SECRET,
     cluster: 'eu',
     useTLS: true
 });
 
-
 (async () => {
-	try {
-		debug(`Starting with config: ${JSON.stringify(config, null, '\t')}`);
+    try {
+        debug(`Starting with config: ${JSON.stringify(config, null, '\t')}`);
 
         let lastImageName = null;
-		while (true) {
-			// take the screenshot every 5 seconds
-			await sleep(config.sleepTime);
+        while (true) {
+            debug(`Sleeping for ${config.sleepTime}...`);
+            await sleep(config.sleepTime);
+
+            debug('Taking screenshot...');
             const imageBuffer = await screenshot();
 
-            //upload buffer with current time appended
-            const imageName =  `screenshot-${uuid()}.png`;
-            const { url: Location } = await s3Client.upload({
-                Bucket: 'system-design-photo-collection',
-                Key: imageName,
-                Body: imageBuffer
-            }).promise();
+            const imageName = `screenshot-${uuid()}.png`;
+            await S3.upload(imageBuffer, imageName);
 
-            // send pusher notification with new url
-            await pusher.trigger('system-design', 'url-change', { url });
+            const url = S3.getSignedUrl(imageName);
+
+            debug(`Sending pusher notification with ${url}...`);
+            await pusher.trigger('system-design', 'screenshot', { url });
 
             if (lastImageName) {
-                // TODO: delete the old image
+                await S3.deleteImage(lastImageName);
             }
             lastImageName = imageName;
-		}
-	} finally {
+        }
+    } finally {
         debug('Shutting down...');
-        // empty the s3 bucket
-	}
+        await S3.emptyBucket();
+    }
 })();
